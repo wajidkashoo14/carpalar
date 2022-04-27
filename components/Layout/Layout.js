0636e24820b14d87b496e888e90cfe2ef689f4ee
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-04-27 15:20:24 +0530

    added new card design

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,10 +5,10 @@ import Navbar from "./Navbar";
 
 function Layout() {
   return (
-    <Box>
+    <>
       <Contactstrip />
       <Navbar />
-    </Box>
+    </>
   );
 }
 
--- components/Layout/Singlecarcard.js
+++ components/Layout/Singlecarcard.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Text, Divider, Button } from "@chakra-ui/react";
 import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -19,34 +19,68 @@ export default function Singlecarcard() {
         router.push("/singlecar");
       }}
     >
-      <Flex flexDir="column" justifyContent="center">
-        <Image src="/homecar.jpg" height={200} width={260} />
+      <Flex flexDir="column" justifyContent="center" p={2}>
+        <Image
+          src="/homecar.jpg"
+          height={200}
+          width={260}
+          borderRadius="5"
+          overflow="hidden"
+        />
+        <Text
+          my="1"
+          mx="2"
+          width="4rem"
+          padding=".1rem"
+          bg="transparent"
+          border="1px solid #4258EF "
+          color="#be9ca1"
+          borderRadius="5"
+          fontSize="14px"
+        >
+          2020
+        </Text>
+        <Text my="1" mr="auto" p="2" fontWeight="600" fontSize="18px">
+          Honda civic sedan
+        </Text>
         <Flex
-          flexDir="column"
-          justifyContent="center"
           bg="white"
           color="#fff"
-          py="3"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          px="2"
         >
-          <Text fontWeight="bold" textAlign="center">
-            Audi RS4
+          <Text fontWeight="bold" textAlign="center" color="#4258EF">
+            $35,000
           </Text>
-
-          <Spacer />
-          <Flex
-            justifyContent="space-between"
-            p="5"
-            textAlign="center"
-            color="gray.400"
-          >
-            <Text>20000</Text>
-            <Text>Diesel</Text>
-            <Text>Automatic</Text>
-          </Flex>
-          <Text fontWeight="bold" textAlign="center">
-            $994,000
+          <Divider orientation="vertical" px={5} />
+          <Text fontWeight="bold" textAlign="center" color="#be9ca1">
+            $299/month
           </Text>
         </Flex>
+        <Spacer />
+        <Flex
+          justifyContent="space-between"
+          p="3"
+          textAlign="center"
+          color="gray.400"
+        >
+          <Text>20000</Text>
+          <Text>Diesel</Text>
+          <Text>Automatic</Text>
+        </Flex>
+        <Button
+          my={2}
+          className="primaryButton"
+          backgroundColor="#4258EF"
+          _hover={{ backgroundColor: "#273edc" }}
+          _focus={{ outline: "none" }}
+          color="white"
+          width="95%"
+          mx="auto"
+        >
+          Rent now
+        </Button>
       </Flex>
     </Box>
   );
