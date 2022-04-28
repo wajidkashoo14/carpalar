236eca99ce995cc901a8b6a6a0bd851fd3187ddc
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-04-28 16:12:03 +0530

    fixed netlify bug

--- components/Layout/Singlecar.js
+++ components/Layout/Singlecar.js
@@ -38,28 +38,28 @@ export default function SIngleCar() {
           $305,000
         </Text>
         <Flex justifyContent="space-between">
-          <Text fontWeight="medium">{`Mileage:&nbsp;`}</Text>
+          <Text fontWeight="medium">Mileage:</Text>
           <Text color="gray.600">25 000 km</Text>
         </Flex>{" "}
         <hr />
         <Flex justifyContent="space-between">
-          <Text fontWeight="medium">{`Transmission:&nbsp;`}</Text>
+          <Text fontWeight="medium">Transmission:</Text>
           <Text color="gray.600">Manual</Text>
         </Flex>
         <hr />
         <Flex justifyContent="space-between">
-          <Text fontWeight="medium">{`Color:&nbsp;`}</Text>
+          <Text fontWeight="medium">Color:</Text>
           <Text color="gray.600">Black</Text>
         </Flex>
         <hr />
         <Flex justifyContent="space-between">
-          <Text fontWeight="medium">{`Body:&nbsp;`}</Text>
+          <Text fontWeight="medium">Body</Text>
           <Text color="gray.600">Hatchback</Text>
         </Flex>
         <hr />
         <Flex justifyContent="space-between">
-          <Text fontWeight="medium">{`Fuel:&nbsp;`}</Text>
-          <Text color="gray.600">Gasoline</Text>
+          <Text fontWeight="medium">Fuel:</Text>
+          <Text color="gray.600">Gasoline:</Text>
         </Flex>
         <Button
           width="fit-content"
