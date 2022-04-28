3503c1f651be764a7fe75975c462611a3f9b2a17
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-04-28 16:21:26 +0530

    fixed netlify bug

--- components/Layout/Productreview.js
+++ components/Layout/Productreview.js
@@ -15,10 +15,10 @@ function Productreview() {
           <Text> Wajid Hussain</Text>
         </Flex>
         <Text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          scrambled it to make a type specimen book.`}
         </Text>
       </Flex>
     </Container>
