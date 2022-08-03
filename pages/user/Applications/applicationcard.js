acf8c13bf1bd78cb5136edee64bf2a005a4009c3
stupidlymoron <officialirfanafzal@gmail.com>
2022-08-03 14:54:32 +0530

    added key

--- pages/user/Applications/applicationcard.js
+++ pages/user/Applications/applicationcard.js
@@ -3,33 +3,33 @@ import React from "react";
 import styles from "../../../styles/dashboard.module.css";
 
 function Applicationcard({ heading, input }) {
-  return (
-    <Flex justifyContent="center" alignItems="center" flexDir="column">
-      <Heading fontSize="1.5rem" p={5}>
-        {heading}
-      </Heading>
-      <Box
-        display="flex"
-        gap="3rem"
-        py={2}
-        mt={2}
-        justifyContent="center"
-        alignItems="center"
-      >
-        {input &&
-          Object.keys(input).map((key) => {
-            return (
-              <Box>
-                <Heading fontSize="1.1rem" py={2}>
-                  {key}
-                </Heading>
-                <p>{input[key]}</p>
-              </Box>
-            );
-          })}
-      </Box>
-    </Flex>
-  );
+	return (
+		<Flex justifyContent="center" alignItems="center" flexDir="column">
+			<Heading fontSize="1.5rem" p={5}>
+				{heading}
+			</Heading>
+			<Box
+				display="flex"
+				gap="3rem"
+				py={2}
+				mt={2}
+				justifyContent="center"
+				alignItems="center"
+			>
+				{input &&
+					Object.keys(input).map((key, idx) => {
+						return (
+							<Box key={idx}>
+								<Heading fontSize="1.1rem" py={2}>
+									{key}
+								</Heading>
+								<p>{input[key]}</p>
+							</Box>
+						);
+					})}
+			</Box>
+		</Flex>
+	);
 }
 
 export default Applicationcard;
