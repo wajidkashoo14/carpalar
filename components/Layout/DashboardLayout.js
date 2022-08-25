36857a30ae9f8acb611bdc992c31c30321b26914
wajidkashoo14 <wajidkashoo14@gmail.com>
2022-08-26 00:25:18 +0530

    as

--- components/Layout/DashboardLayout.js
+++ components/Layout/DashboardLayout.js
@@ -3,27 +3,24 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import NavigationItems from "../UserDashBoard/NavigationItems";
 import Cookies from "js-cookie";
-import { RelationCountAttribute } from "typeorm/query-builder/relation-count/RelationCountAttribute";
 
 const DashboardLayout = ({ children }) => {
-	const router = useRouter();
-	useEffect(() => {
-		const user = Cookies.get("user")
-			? JSON.parse(Cookies.get("user"))
-			: null;
-		const token = Cookies.get("token")
-			? JSON.parse(Cookies.get("token"))
-			: null;
-		if (!token || !user) {
-			router.push("/signin");
-		}
-	}, []);
-	return (
-		<Flex pt="1vh" w="100vw" justifyContent="flex-start">
-			<NavigationItems />
-			{children}
-		</Flex>
-	);
+  const router = useRouter();
+  useEffect(() => {
+    const user = Cookies.get("user") ? JSON.parse(Cookies.get("user")) : null;
+    const token = Cookies.get("token")
+      ? JSON.parse(Cookies.get("token"))
+      : null;
+    if (!token || !user) {
+      router.push("/signin");
+    }
+  }, []);
+  return (
+    <Flex pt="1vh" w="100vw" justifyContent="flex-start">
+      <NavigationItems />
+      {children}
+    </Flex>
+  );
 };
 
 export default DashboardLayout;
